42dc72f75949bd0361ac3a6012688bbf352bb0dc
joinalzx <joinalzx@gmail.com>
2024-01-22 17:42:04 +0600

    conditional CSS and style

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
    const {name, flags, population, area, cca3} = country;
    
    const [visited, setVisited] = useState(false);
@@ -9,6 +9,8 @@ const Country = ({country}) => {
         setVisited(!visited);
     }
 
+    console.log(handleVisitedCountry);
+
    return (
         <div className={`country ${visited ? 'visited': 'non-visited'}`}>
             <h3 style={{color: visited ? 'Purple' : 'Blue'}}>{name.common}</h3>
@@ -16,6 +18,8 @@ const Country = ({country}) => {
             <p>Population: {population}</p>
             <p>area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button>Mark Visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'visited' : 'Will Go'}</button>
             {visited && 'I have visited this country.'}
         </div>
