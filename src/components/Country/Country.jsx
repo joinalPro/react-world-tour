54f318b8ad7692ee3d12981ff72e8d3b06291eb2
joinalzx <joinalzx@gmail.com>
2024-01-22 17:13:45 +0600

    react world tour

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './Country.css'
+const Country = ({country}) => {
+   const {name, flags, population, area, cca3} = country;
+   
+   const [visited, setVisited] = useState(false);
+   
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
+   return (
+        <div className={`country ${visited ? 'visited': 'non-visited'}`}>
+            <h3 style={{color: visited ? 'Purple' : 'Blue'}}>{name.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited}>{visited ? 'visited' : 'Will Go'}</button>
+            {visited && 'I have visited this country.'}
+        </div>
+    );
+};
+
+export default Country;
